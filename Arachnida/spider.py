59c24d9f8ae2e58f0f2765a747efbef9e01751ae
hkrifa <haroun_krifa@yahoo.fr>
2023-10-06 16:45:52 +0200

    modif spider.py

--- Arachnida/spider.py
+++ Arachnida/spider.py
@@ -11,7 +11,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
 }
 pattern = r'^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$'
-            
+
 def args_parser() ->  argparse.Namespace:
     parser = argparse.ArgumentParser(description="The spider program allow you to extract all the images from a website, recursively, by providing a url as a parameter")
     parser.add_argument("URL", help='url to be scraped', type=str)
@@ -20,7 +20,6 @@ def args_parser() ->  argparse.Namespace:
     parser.add_argument("-p", "--path", help=": indicates the path where the downloaded files will be saved. If not specified, ./data/ will be used", type=str, default="./data/")
 
     args = parser.parse_args()
-   
     return args
 
 def print_params(args) -> None:
@@ -35,8 +34,8 @@ def get_file_name(path, f_id) -> str:
     listlen = len(split_parsed_path)
     return str(f_id) + '-' + split_parsed_path[listlen - 1]
 
-def get_total_image(args, level):
-    nb_images = 0
+def get_src_urls(args, level) -> list:
+    src_urls = []
 
     for i in range(1, level + 1):
         sys.stdout.write(f'\rProcessing page {i}/{level}')
@@ -49,16 +48,16 @@ def get_total_image(args, level):
             if 'src' in image.attrs:
                 src = html.unescape(image['src'])
                 if re.match(pattern, src):
-                    nb_images += 1
-    return nb_images
+                    src_urls.append(src)
+    return src_urls
 
-def progress_bar(downloaded, level, total):
-    bar_length = 50
+def progress_bar(downloaded, total):
+    bar_length = 100
     progress = (downloaded / total)
     barre = '=' * int(round(progress * bar_length)-1)
     spaces = ' ' * (bar_length - len(barre))
     
-    sys.stdout.write('\rDownloaded level {}: [{}] {:.2f}%'.format(level, barre + spaces, progress * 100))
+    sys.stdout.write('\rDownload images: [{}] {:.2f}%'.format(barre + spaces, progress * 100))
     sys.stdout.flush()
 
 def spider(args) -> None:
@@ -67,28 +66,21 @@ def spider(args) -> None:
 
     if not os.path.exists(args.path):
         os.mkdir(args.path)
-    total_images = get_total_image(args=args, level=level)
+    src_urls = get_src_urls(args=args, level=level)
+    total_images = len(src_urls)
     time.sleep(0.5)
-    for i in range(1, level + 1):
-        url = str(args.URL) + f"?page={i}"
-        response = requests.get(url=url, headers=headers, stream=True)
-        if response.ok:
-            f_id = 0
-            soup = BeautifulSoup(response.text, 'html.parser')
-            images = soup.findAll('img')
-            for image in images:
-                 if 'src' in image.attrs:
-                    src = html.unescape(image['src'])
-                    if re.match(pattern, src):
-                        filename = get_file_name(path=src, f_id=f_id)
-                        data = requests.get(src).content
-                        f = open(args.path + filename, 'wb')
-                        f.write(data)
-                        f.close()
-                        f_id += 1
-                        downloaded += 1
-                        progress_bar(downloaded=downloaded, level=i, total=total_images)
-                        time.sleep(0.1)
+    print('\n')
+    f_id = 0
+    for src_url in src_urls:
+        filename = get_file_name(path=src_url, f_id=f_id)
+        data = requests.get(src_url).content
+        f = open(args.path + filename, 'wb')
+        f.write(data)
+        f.close()
+        f_id += 1
+        downloaded += 1
+        progress_bar(downloaded=downloaded, total=total_images)
+        time.sleep(0.1)                 
 
 def main():
     args = args_parser()
